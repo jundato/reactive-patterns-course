7b7b9b9c23de7f2ce72c11cbd5673e5540cb61ee
Virgilio T. Dato <virgiliodato@gmail.com>
2018-03-26 10:52:47 +0800

    browser-event-experiments

---
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'browser-event-experiments',
+  templateUrl: './browser-event-experiments.component.html',
+  styleUrls: ['./browser-event-experiments.component.css']
+})
+export class BrowserEventExperimentsComponent implements OnInit {
+
+  hoverSection: HTMLElement;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.hoverSection = document.getElementById('hover');
+
+    this.hoverSection.addEventListener('mousemove', onMouseMove);
+  }
+
+  unsubscribe() {
+    this.hoverSection.removeEventListener('mousemove', onMouseMove);
+  }
+
+}
+
+function onMouseMove(event) {
+  console.log(event);
+}
